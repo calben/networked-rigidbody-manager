83ab27079ab2dcb42bbaf9d7fe7627af3c1959d2
Calem Bendell <calem.j.bendell@gmail.com>
2015-02-21 14:03:03 -0500

    initialised project files

---
+++ Unity/Assets/Scripts/DeftNetwork.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeftNetwork : MonoBehaviour
+{
+  private const string typeName = "DeftNetwork";
+  private const string gameName = "DeftRoom";
+
+  private bool refreshHosts = false;
+  private bool isJoining = false;
+  private bool isHosting = false;
+  private HostData[] hostList;
+
+  private GameObject selectedPlayer = null;
+
+
+  void Start()
+  {
+    Application.runInBackground = true;
+  }
+
+  void OnGUI()
+  {
+    if (!Network.isClient && !Network.isServer)
+      if (isJoining && selectedPlayer != null)
+      {
+        if (hostList != null)
+          for (int i = 0; i < hostList.Length; i++)
+            if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+              JoinServer(hostList[i]);
+      }
+      else if (isHosting && selectedPlayer != null)
+      {
+        HostServer();
+      }
+      else if (selectedPlayer == null && (isHosting || isJoining))
+      {
+        // enable player select
+        this.GetComponent<PlayerSelect>().enabled = true;
+        this.selectedPlayer = this.GetComponent<PlayerSelect>().selectedPlayer;
+      }
+      else
+      {
+        if (GUI.Button(new Rect(100, 100, 300, 100), "Start Server"))
+          isHosting = true;
+        if (GUI.Button(new Rect(100, 250, 300, 100), "Refresh Hosts to Join"))
+        {
+          isJoining = true;
+          RefreshHostList();
+        }
+      }
+  }
+
+  private void HostServer()
+  {
+    Network.InitializeServer(16, 25000, false);
+    MasterServer.RegisterHost(typeName, gameName);
+  }
+
+  void OnServerInitialized()
+  {
+    this.GetComponent<PlayerSelect>().SpawnPlayer();
+  }
+
+  void FixedUpdate()
+  {
+    if (refreshHosts && MasterServer.PollHostList().Length > 0)
+    {
+      refreshHosts = false;
+      hostList = MasterServer.PollHostList();
+    }
+  }
+
+  private void RefreshHostList()
+  {
+    if (!refreshHosts)
+    {
+      refreshHosts = true;
+      MasterServer.RequestHostList(typeName);
+    }
+  }
+
+  private void JoinServer(HostData hostData)
+  {
+    Network.Connect(hostData);
+  }
+
+  void OnConnectedToServer()
+  {
+    this.GetComponent<PlayerSelect>().SpawnPlayer();
+  }
+
+}
---
+++ Unity/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectManager : MonoBehaviour
+{
+
+    public List<GameObject> trackedObjects;
+    public int layer;
+    public bool showDebugMessage;
+    public bool colorSyncedObjects;
+
+
+    float timeBetweenSync = 0.1f;
+    float timeBetweenAllSync = 1.0f;
+
+    float lastSyncTime;
+    float lastAllSyncTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (NetworkView n in GetComponents<NetworkView>())
+            n.observed = this;
+        trackedObjects = new List<GameObject>();
+        foreach (GameObject obj in FindObjectsOfType<GameObject>())
+        {
+            if (obj.layer == layer)
+            {
+                trackedObjects.Add(obj);
+            }
+        }
+        Debug.Log("Tracking " + trackedObjects.Count + " objects for automated networking.");
+    }
+
+    /// <summary>
+    /// These variables are class fields for efficiency purposes when serialising.
+    /// </summary>
+    Vector3 pos;
+    Quaternion rot;
+    Vector3 velocity;
+    Vector3 angularVelocity;
+    
+    void SyncObject()
+    {
+
+    }
+
+    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        Debug.Log(lastSyncTime);
+        Debug.Log(Time.time);
+        if (Time.time - lastSyncTime > timeBetweenSync)
+        {
+            Debug.Log("Syncing set.");
+            lastSyncTime = Time.time;
+            foreach (GameObject obj in trackedObjects)
+            {
+                //if (obj.GetComponent<InterpolatedPropertySet>().hasMovedBeyondThreshold() || true)
+                {
+                    Debug.Log("Syncing movement data for " + obj.name);
+                    if (stream.isWriting)
+                    {
+                        pos = obj.rigidbody.position;
+                        rot = obj.rigidbody.rotation;
+                        velocity = obj.rigidbody.velocity;
+                        angularVelocity = obj.rigidbody.angularVelocity;
+
+                        stream.Serialize(ref pos);
+                        stream.Serialize(ref velocity);
+                        stream.Serialize(ref rot);
+                        stream.Serialize(ref angularVelocity);
+                    }
+                    else
+                    {
+                        Vector3 pos = Vector3.zero;
+                        Vector3 velocity = Vector3.zero;
+                        Quaternion rot = Quaternion.identity;
+                        Vector3 angularVelocity = Vector3.zero;
+                        stream.Serialize(ref pos);
+                        stream.Serialize(ref velocity);
+                        stream.Serialize(ref rot);
+                        stream.Serialize(ref angularVelocity);
+                        obj.transform.position = pos;
+                        obj.rigidbody.velocity = velocity;
+                        obj.rigidbody.rotation = rot;
+                        obj.rigidbody.angularVelocity = angularVelocity;
+                    }
+                }
+            }
+        }
+    }
+}
